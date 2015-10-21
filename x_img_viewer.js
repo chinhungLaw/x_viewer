0b3aa976f2a48557d0ba72a5c09e9c6c344452d2
chinhungLaw <adamluo2003@qq.com>
2015-10-21 18:34:27 +0800

    edit

--- x_img_viewer.js
+++ x_img_viewer.js
@@ -4,7 +4,7 @@
  * 描述:此插件自动适应页面缩放,自动生成样式和DOM节点,只需在父级DOM添加 id="x_img_viewer" 即可使用,并支持显示高清大图(用法详见下文),.
  * 用法: 1.只需在父级DOM添加 id="x_img_viewer" ,本插件便会自动搜索此父级DOM下的全部 img DOM节点.
 		 2.如需显示高清图,只需在img DOM节点下添加属性 x_img_viewer_url="{path}" 即可.
- * @author luozx
+ * @author chinhungLaw
  * @Email adamluo2003@qq.com
  * @version 1.0.0
  * @dependencies none
